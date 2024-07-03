84db9dcdf9eef7c65b5e979f17284554c9b3a4f1
s-pugh <samanthalpugh@gmail.com>
2024-07-03 13:08:40 -0500

    exercises finished

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,21 +4,60 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+for (let i = 0; i < 21; i++){
+  console.log(i)
+}
+
+for (let j = 3; j < 30; j = j+2){
+  console.log(j);
+}
+
+for (let k = 12; k > -15; k = k-2){
+  console.log(k)
+}
+
+for (let l = 50; l > 20; l--){
+
+  if (0 === (l % 3)){
+    console.log(l)
+  }
+}
+
+//*Exercise #2: 
+//Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+
+let one = "LaunchCode";
+let two = [1, 5, 'LC101', 'blue', 42];
+console.log(one);
+console.log(two);
+
+//Construct ``for`` loops to accomplish the following tasks:
+ // a. Print each element of the array to a new line.
+  //b. Print each character of the string - in reverse order - to a new line. */ 
+
+for (let i =  0; i < two.length; i++){
+  console.log(two[i]);
+}
 
 
-
-/*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
-
-Construct ``for`` loops to accomplish the following tasks:
-  a. Print each element of the array to a new line.
-  b. Print each character of the string - in reverse order - to a new line. */ 
-
-
+for (let j = 9; j >= 0; j--){
+console.log(one[j]);
+}
 
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+let even = []
+let odd = []
+  let three = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104]
+  for (k = 0; k < three.length; k++){
+    if (three[k] % 2 === 0){
+      even.splice(0, 0, three[k])
+    } else if (three[k] % 2 === 1) {
+      odd.splice(0, 0, three[k])
+    }
+  }
+  console.log(even);
+  console.log(odd);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,46 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+const input = require('readline.sync')
 
-
+let startingFuelLevel = 20000
+let numberOfAstronauts = 6
+let totalAltitude = 0
 
 
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-
+while (startingFuelLevel <= 5000 || startingFuelLevel >= 30000 || isNaN(startingFuelLevel)) {
+  startingFuelLevel = input.question("What is the starting fuel level?")
+  } 
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+ while (numberOfAstronauts >= 7 || numberOfAstronauts <= 1 || isNaN(numberOfAstronauts)) {
+  numberOfAstronauts = input.question("How many astronauts are on board?")
+  }
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+while(startingFuelLevel-100*numberOfAstronauts >= 0){
+  startingFuelLevel -= 100*numberOfAstronauts
+  totalAltitude += 50
+  return totalAltitude
+}
 
+let output= `The shuttle gained an elevation of ${totalAltitude} km.`
 
+if (totalAltitude >= 2000) {
+  output += " Orbit achieved!"
+}
+
+console.log(output)
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
-If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit*/
+
+
+
