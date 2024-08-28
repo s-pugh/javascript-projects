ae20031116233d45eceba0b6ba2f6c5337c5dbad
s-pugh <samanthalpugh@gmail.com>
2024-08-28 14:51:42 -0500

    exercise completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,20 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function(event){
+        paragraph.innerHTML = 'Houston! We have liftoff!'
+    })
+    missionAbort.addEventListener("mouseover", function(event){
+        event.target.style.backgroundColor = "red"
+    })
+    missionAbort.addEventListener("mouseout", function(event){
+        event.target.style.backgroundColor = ""
+    })
+    missionAbort.addEventListener("click", function(event){
+       if( window.confirm("Are you sure you want to abort the mission?")) {
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+       }
+    })
 }
 
 window.addEventListener("load", init);
